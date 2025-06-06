f73c16f1f6fe659838595880b708fe7a497f8470
sumadvena <mat.wladyka@gmail.com>
2025-06-06 08:26:22 +0200

    initial

---
+++ utils.py
@@ -0,0 +1,14 @@
+def add(a: int, b: int) -> int:
+    return a + b
+
+
+def substract(a: int, b: int) -> int:
+    return a - b
+
+
+def multiply(a: int, b: int) -> int:
+    return a * b
+
+
+def divide(a: int, b: int) -> float:
+    return a / b
